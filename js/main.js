674e75435455192360f7a18f43bca42d2adb5201
hellomagmile <davidalcoentre@gmail.com>
2025-08-02 16:19:31 -0400

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const talent=document.querySelector('#talent');
-if(talent){
-  talent.addEventListener('mouseenter',()=>{
-    talent.innerHTML='COMING&nbsp;SOON<br>即将上线';
-    talent.style.pointerEvents='none';
-  });
-}
